3ef4c78f0e26133cda8fc97f4bdb09a99aa4b20e
AIdan19 <aydenfakirmohamed@gmail.com>
2020-06-17 13:27:38 +0000

    fixed direct game over
    Signed-off-by: AIdan19 <aydenfakirmohamed@gmail.com>

--- script.js
+++ script.js
@@ -28,14 +28,13 @@ var spelerY = 100; // y-positie van speler
 var kogelX = 0;    // x-positie van kogel
 var kogelY = 0;    // y-positie van kogel
 
-var vijandX = 0;   // x-positie van vijand
-var vijandY = 0;   // y-positie van vijand
+var vijandX = 50;   // x-positie van vijand
+var vijandY = 100;   // y-positie van vijand
 
 var score = 0; // aantal behaalde punten
 
-
-
-
+var imgA=0;
+var imgB=0;
 
 /* ********************************************* */
 /*      functies die je gebruikt in je game      */
@@ -50,16 +49,6 @@ var tekenVeld = function () {
   rect(20, 20, width - 2 * 20, height - 2 * 20);
 };
 
-var tekenVijand = function(x,  y) {
-  fill(20,230,230);
-  ellipse(vijandX ,  vijandY, 50, 50); //hoofd
-  fill(200,30,30);
-  ellipse(vijandX-25, vijandY+25, 20, 20); //oog links
-  ellipse(vijandX+25, vijandY+25, 20, 20); //oog rechts
-  ellipse(vijandX,    vijandY-25, 75, 20); //mond
-
-};
-
 /**
  * Tekent de kogel of de bal
  * @param {number} x x-coördinaat
@@ -70,61 +59,50 @@ var tekenKogel = function(x, y) {
 }
 
 /**
+ * Tekent de vijand
+ * @param {number} x x-coördinaat
+ * @param {number} y y-coördinaat
+ */
+var tekenVijand = function(x,  y) {
+  /*
+  fill(20,230,230);
+  ellipse(vijandX ,  vijandY, 50, 50); //hoofd
+  fill(200,30,30);
+  ellipse(vijandX-25, vijandY+25, 20, 20); //oog links
+  ellipse(vijandX+25, vijandY+25, 20, 20); //oog rechts
+  ellipse(vijandX,    vijandY-25, 75, 20); //mond
+  */
+  image(imgB, x, y, 50, 50); 
+};
+
+/**
  * Tekent de speler
  * @param {number} x x-coördinaat
  * @param {number} y y-coördinaat
  */
-
- var imgA=0;
-var imgB=0;
-
-
-function preload() {
-  imgA = loadImage("afbeeldingen/lion.png"); 
-  imgB = loadImage("afbeeldingen/zebra.png");
-}
-
 var tekenSpeler = function(x, y) {
-  
-
+   image(imgA, x, y, 800, 450); 
 };
 
 /**
  * Updatet globale variabelen met positie van vijand of tegenspeler
  */
 var beweegVijand = function() {
-if (keyIsDown(68) && vijandX < 1200)
+  if (keyIsDown(68) && vijandX < 1200) { // d
+    vijandX = vijandX + 3;
+  }
 
-vijandX = vijandX + 3;
+  if (keyIsDown(65) && vijandX > 60) { // a
+    vijandX = vijandX - 3;
+  }
 
-else 
+  if (keyIsDown(83) && vijandY < 660) { // s
+    vijandY = vijandY + 3;
+  }
 
-vijandX = vijandX + 0;
-
-if (keyIsDown(65) && vijandX > 60)
-
-vijandX = vijandX - 3;
-    
-
-else 
-
-vijandX = vijandX - 0;
-
-if (keyIsDown(83) && vijandY < 660)
-
-vijandY = vijandY + 3;
-
-else
-
-vijandY = vijandY + 0;
-
-if (keyIsDown(87) && vijandY > 60)
-
-vijandY = vijandY - 3;
-
-else
-
-vijandY = vijandY - 0;
+  if (keyIsDown(87) && vijandY > 60) { // w
+    vijandY = vijandY - 3;
+  }
 };
 
 
@@ -141,45 +119,24 @@ var beweegKogel = function() {
  * Updatet globale variabele spelerX en spelerY
  */
 var beweegSpeler = function() {
-if (keyIsDown(39) && spelerX < 1220)
+    console.log("beweegSpeler");
+  if (keyIsDown(39) && spelerX < 1220) { // pijlrechts
+   spelerX = spelerX + 3;
+   console.log("speler:rechts");
+  }
 
-spelerX = spelerX + 3;
+  if (keyIsDown(37) && spelerX > 60) { // pijllinks
+    spelerX = spelerX - 3;
+  }
 
-else 
+  if (keyIsDown(40) && spelerY < 660) { // pijlonder
+    spelerY = spelerY + 3;
+  }
 
-spelerX = spelerX + 0;
-
-if (keyIsDown(37) && spelerX > 60)
-
-spelerX = spelerX - 3;
-    
-
-else 
-
-spelerX = spelerX - 0;
-
-if (keyIsDown(40) && spelerY < 660)
-
-spelerY = spelerY + 3;
-
-else
-
-spelerY = spelerY + 0;
-
-if (keyIsDown(38) && spelerY > 60)
-
-spelerY = spelerY - 3;
-
-else
-
-spelerY = spelerY - 0;
+  if (keyIsDown(38) && spelerY > 60) { //pijlboven
+    spelerY = spelerY - 3;
+  }
 };
-
-
-
-
-
-
 
 /**
  * Zoekt uit of de vijand is geraakt
@@ -190,19 +147,15 @@ var checkVijandGeraakt = function() {
   return false;
 };
 
-
 /**
  * Zoekt uit of de speler is geraakt
  * bijvoorbeeld door botsing met vijand
  * @returns {boolean} true als speler is geraakt
  */
 var checkSpelerGeraakt = function() {
-    if (checkSpelerGeraakt) {   
-        spelStatus = GAMEOVER;
-      }
+
   return false;
 };
-
 
 /**
  * Zoekt uit of het spel is afgelopen
@@ -213,6 +166,15 @@ var checkGameOver = function() {
   return false;
 };
 
+/**
+ * preload
+ * de code in deze functie wordt één keer uitgevoerd door
+ * de p5 library, voordat setup wordt aangeroepen
+ */
+function preload() {
+  imgA = loadImage("afbeeldingen/lion.png"); 
+  imgB = loadImage("afbeeldingen/zebra.png");
+}
 
 /**
  * setup
@@ -234,8 +196,10 @@ function setup() {
  * uitgevoerd door de p5 library, nadat de setup functie klaar is
  */
 function draw() {
+    console.log("draw");
   switch (spelStatus) {
     case SPELEN:
+        console.log("SPELEN");
       beweegVijand();
       beweegKogel();
       beweegSpeler();
@@ -254,11 +218,6 @@ function draw() {
       tekenVijand(vijandX, vijandY);
       tekenKogel(kogelX, kogelY);
       tekenSpeler(spelerX, spelerY);
-
-
-  image(imgA, spelerX, spelerY, 800, 450); 
-  image(imgB, vijandX, vijandY, 50, 50); 
-
 
       if (checkGameOver()) {
         spelStatus = GAMEOVER;
